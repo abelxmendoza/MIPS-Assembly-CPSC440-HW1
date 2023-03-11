fb4f05665357eee85773e09373f975256bd54f4c
abelxmendoza <abelxmendoza@gmail.com>
2023-03-11 11:44:37 -0800

    finished addmem.asm

--- addmem.asm
+++ addmem.asm
@@ -1,60 +1,61 @@
-# Data segment
 .data
-promptA: .asciiz "Enter the value of A: "
-promptB: .asciiz "Enter the value of B: "
-result: .asciiz "The result is: "
-
-# Memory variables
+prompt1: .asciiz "Enter value for A: "
+prompt2: .asciiz "Enter value for B: "
+result: .asciiz "Result: "
+newline: .asciiz "\n"
 A: .word 0
 B: .word 0
 C: .word 0
+prompt: .asciiz "Do you want to continue? (1: Yes, 0: No) "
 
-# Text segment
 .text
+.globl main
 main:
-    # Prompt the user to enter A and B
-    li $v0, 4
-    la $a0, promptA
-    syscall
-    
-    li $v0, 5
-    syscall
-    sw $v0, A
-    
-    li $v0, 4
-    la $a0, promptB
-    syscall
-    
-    li $v0, 5
-    syscall
-    sw $v0, B
-    
-    # Add A and B and store the result in C
-    lw $t0, A
-    lw $t1, B
-    addmem $t2, $t0, $t1
-    sw $t2, C
-    
-    # Print the result
-    li $v0, 4
-    la $a0, result
-    syscall
-    
-    lw $a0, C
-    li $v0, 1
-    syscall
-    
-    # Prompt the user to continue or exit
-    li $v0, 4
-    la $a0, "Do you want to continue? (y/n): "
-    syscall
-    
-    li $v0, 12
-    syscall
-    
-    beq $v0, 110, exit # 110 is ASCII code for 'n'
-    j main
-    
+li $v0, 4           # print prompt1
+la $a0, prompt1
+syscall
+
+li $v0, 5           # read A
+syscall
+sw $v0, A
+
+li $v0, 4           # print prompt2
+la $a0, prompt2
+syscall
+
+li $v0, 5           # read B
+syscall
+sw $v0, B
+
+lw $t0, A           # load A into $t0
+lw $t1, B           # load B into $t1
+add $t2, $t0, $t1    # C = A + B
+sw $t2, C           # store C
+
+li $v0, 4           # print result
+la $a0, result
+syscall
+
+lw $a0, C           # load C into $a0
+li $v0, 1           # print integer
+syscall
+
+li $v0, 4           # print newline
+la $a0, newline
+syscall
+
+# loop
+loop:
+li $v0, 4           # print prompt
+la $a0, prompt
+syscall
+
+li $v0, 5           # read input
+syscall
+beq $v0, 0, exit    # if input == 0, exit
+
+j main
+
 exit:
-    li $v0, 10
-    syscall
+li $v0, 10          # exit program
+syscall
